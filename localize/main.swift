441dab5411c615f5dcea6fe93a207be1370cdbe8
Pirush Prechathavanich <pirush@nimbl3.com>
2019-02-15 11:30:23 +0700

    Re-organize file organization

--- localize/main.swift
+++ localize/main.swift
@@ -6,8 +6,26 @@
 //  Copyright © 2018 Nimbl3. All rights reserved.
 //
 
+import Foundation
+
+final class CommandLineTool {
+    
+    let arguments: [String]
+    
+    init(arguments: [String] = CommandLine.arguments) {
+        self.arguments = arguments
+    }
+    
+    func run() throws {
+        Runner().run()
+    }
+    
+}
+
+let tool = CommandLineTool()
+
 do {
-    try Runner().run()
+    try tool.run()
 } catch {
     print("\n ### ⚠️ Error: \(error) ### \n")
 }
