ddf5083122fda928370b21f048226342a60666b5
Pirush Prechathavanich <pirush.t@gmail.com>
2019-03-22 14:22:16 +0700

    add AEXML as a dependency

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "localize",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/tadija/AEXML.git", from: "4.3.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "localize",
-            dependencies: []),
+            dependencies: ["AEXML"]),
         .testTarget(
             name: "localizeTests",
             dependencies: ["localize"]),
